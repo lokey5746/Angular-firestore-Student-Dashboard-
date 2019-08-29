8573b86923e7dc55857b3e36f02554a48df72e61
Lokendra Dangi <pachar.com@gmail.com>
2019-08-29 22:38:19 +0545

    Firebase connected to Angular app

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { AngularFireModule } from '@angular/fire';
+import{ AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyArHiTC395XjYoVfZEqfup0DxWjOAL0wbw",
+    authDomain: "angular-firestore-fadb0.firebaseapp.com",
+    databaseURL: "https://angular-firestore-fadb0.firebaseio.com",
+    projectId: "angular-firestore-fadb0",
+    storageBucket: "angular-firestore-fadb0.appspot.com",
+    messagingSenderId: "195614338243",
+    appId: "1:195614338243:web:0ca5f0119e886352"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
