dcc2baccb7c1344a6677eebd6233eb4993cf630e
Lokendra Dangi <pachar.com@gmail.com>
2019-08-31 22:55:36 +0545

    Data is read to employee list and updated is also done

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/shared/employee.service';
+import { Employee } from 'src/app/shared/employee.model';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor() { }
+  list: Employee[];
+  constructor(private service: EmployeeService) { }
 
   ngOnInit() {
+    this.service.getEmployee().subscribe(actionArray =>{
+      this.list = actionArray.map(item =>{
+        return {
+          id:item.payload.doc.id,
+          ...item.payload.doc.data()} as Employee
+      })
+    })
+  }
+
+  onEdit(emp:Employee){
+    this.service.formData = Object.assign({},emp);
+    
   }
 
 }
--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -33,8 +33,12 @@ export class EmployeeComponent implements OnInit {
   }
 
   onSubmit(form:NgForm){
-    let data = form.value;
+    let data = Object.assign({}, form.value);
+    delete data.id;
+    if(form.value.id == null)
     this.firestore.collection('employees').add(data);
+    else
+    this.firestore.doc('employees/' + form.value.id).update(data);
     this.resetForm(form);
     this.toaster.success("submitted Sucessfully","Std Register");
   }
